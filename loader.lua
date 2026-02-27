dc42ee96039bd631f7702becdd7c14d68757aa5b
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2026-02-27 14:40:39 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,4 @@
-local Loaded: boolean = game:IsLoaded()
-if not Loaded then
-	repeat task.wait() until game:IsLoaded()
-	task.wait(6)
-end
+repeat task.wait() until game:IsLoaded()
 local Args = ...
 local commit = Args.Commit
 shared.VapeDeveloper = shared.VapeDeveloper or Args.Developer
