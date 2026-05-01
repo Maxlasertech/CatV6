b084641f7111bc37ed18d1760b0540a3dd675c67
MaxlaserTech <135626624+Maxlasertech@users.noreply.github.com>
2026-05-02 01:05:48 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -104,7 +104,7 @@ local function finishLoading()
 	end))
 
 	if not vape.Categories then return end
-	if vape.Place ~= 6872274481 then
+	if vape.Place ~= 6872274481 and vape.Notifications.Enabled then
 		task.spawn(function()
 			local body = httpService:JSONEncode({
 				nonce = httpService:GenerateGUID(false),
@@ -117,7 +117,7 @@ local function finishLoading()
 
 			for i = 1, 2 do
 				task.spawn(function()
-					--[[request({
+					request({
 						Method = 'POST',
 						Url = 'http://127.0.0.1:6463/rpc?v=1',
 						Headers = {
@@ -125,7 +125,7 @@ local function finishLoading()
 							Origin = 'https://discord.com'
 						},
 						Body = body
-					})]]
+					})
 				end)
 			end
 		end)
