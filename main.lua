2138e26d40e35ae0fbc5f1cdc3cf81b8fa74d592
MaxlaserTech <135626624+Maxlasertech@users.noreply.github.com>
2026-07-27 11:18:08 +0700

    fr

--- main.lua
+++ main.lua
@@ -123,7 +123,6 @@ local function finishLoading()
 	end
 end
 
-downloadFile('catrewrite/libraries/pathfind.lua')
 if not isfile('catrewrite/profiles/gui.txt') then
 	writefile('catrewrite/profiles/gui.txt', 'new')
 end
