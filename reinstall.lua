6b24d68e47660bdc23c8b7790f87108972e06973
MaxlaserTech <135626624+Maxlasertech@users.noreply.github.com>
2026-08-17 07:05:14 +0700

    Update reinstall.lua

--- reinstall.lua
+++ reinstall.lua
@@ -1,13 +1,4 @@
-local downloader = Instance.new('TextLabel')
-downloader.Size = UDim2.new(1, 0, 0, 40)
-downloader.BackgroundTransparency = 1
-downloader.TextStrokeTransparency = 0
-downloader.TextSize = 20
-downloader.TextColor3 = Color3.new(1, 1, 1)
-downloader.Font = Enum.Font.Arial
-downloader.Text = 'Reinstalling script..'
-downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(game:GetService('CoreGui')))
-task.delay(0.1, downloader.Destroy, downloader)
-
-delfolder('catsix')
-loadstring(game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/main/init.lua'), 'init.lua')(...)
+if isfolder('catsix') then
+    delfolder('catsix')
+end
+cloneref(game:GetService('Players')).LocalPlayer:Kick('Reinstalled, head over to the script panel to get the main script')
